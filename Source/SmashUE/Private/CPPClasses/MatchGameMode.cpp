dff6dea29bfc6cee9e31de1e5932baaf7578d8b4
AeroPorc <hugopv.smp@gmail.com>
2024-11-06 10:39:35 +0100

    Comiting time 2

--- Source/SmashUE/Private/CPPClasses/MatchGameMode.cpp
+++ Source/SmashUE/Private/CPPClasses/MatchGameMode.cpp
@@ -2,3 +2,37 @@
 
 
 #include "CPPClasses/MatchGameMode.h"
+#include "Arena/ArenaPlayerStart.h"
+#include "Kismet/GameplayStatics.h"
+
+void AMatchGameMode::FindPlayerStartActorInArena(TArray<AArenaPlayerStart*>& ResultActors)
+{
+	TArray<AActor*> FoundActors;
+	UGameplayStatics::GetAllActorsOfClass(GetWorld(),AArenaPlayerStart::StaticClass(), FoundActors);
+
+	for(int i = 0; i < FoundActors.Num(); i++)
+	{
+		AArenaPlayerStart* ArenaPlayerStartActor = Cast<AArenaPlayerStart>(FoundActors[i]);
+		if(ArenaPlayerStartActor == nullptr) continue;
+
+		ResultActors.Add(ArenaPlayerStartActor);
+	}
+}
+
+void AMatchGameMode::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	TArray<AArenaPlayerStart*> PlayerStartsPoints;
+	FindPlayerStartActorInArena(PlayerStartsPoints);
+
+	for (AArenaPlayerStart* PlayerStart : PlayerStartsPoints)
+	{
+		GEngine->AddOnScreenDebugMessage(
+			-1,
+			3.f,
+			FColor::Red,
+			PlayerStart->GetFName().ToString()
+		);
+	}
+}
